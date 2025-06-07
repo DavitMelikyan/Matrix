8fe4adeb39940878795f969ce6c86ad02a4eaeff
Davit Melikyan <davidmelikyan900@gmail.com>
2025-06-07 17:13:31 +0400

    Matrix

--- matr.cpp
+++ matr.cpp
@@ -1,5 +1,30 @@
 #include "matrix.h"
 
+Matrix::Matrix() {
+        rows_ = 0;
+	cols_ = 0;
+        data_ = nullptr;
+}
+
+Matrix::Matrix(size_t rows, size_t cols) {
+        rows_ = rows;
+       	cols_ = cols;
+        data_ = new double*[rows_];
+        for (int i = 0; i < rows_; ++i) {
+      	        data_[i] = new double[cols_];
+        }
+}
+
+Matrix::~Matrix() {
+        for (int i = 0; i < rows_; ++i) {
+ 		delete [] data_[i];
+        }
+	rows_ = 0;
+	cols_ = 0;
+        delete [] data_;
+        data_ = nullptr;
+}
+
 void Matrix::set(size_t row, size_t col, double value) {
  	if (row > rows_ - 1 || col > cols_  - 1) {
                 std::cout << "Invalid position in matrix\n";
--- matrix.h
+++ matrix.h
@@ -4,33 +4,14 @@
 
 class Matrix {
 public:	
-	Matrix() {
-		rows_ = 0;
-		cols_ = 0;
-		data_ = nullptr;
-	}
-	Matrix(size_t rows, size_t cols) {
-		rows_ = rows;
-		cols_ = cols;
-		data_ = new double*[rows_];
-		for (int i = 0; i < rows_; ++i) {
-			data_[i] = new double[cols_];
-		}
-	}
+	Matrix();
+	Matrix(size_t rows, size_t cols);
 	void set(size_t row, size_t col, double value);
 	double get(size_t row, size_t col) const;
 	size_t getRows() const;
 	size_t getCols() const;
 	void print() const;
-	~Matrix() {
-		rows_ = 0;
-		cols_ = 0;
-		for (int i = 0; i < rows_; ++i) {
-			delete [] data_[i];
-		}
-		delete data_;
-		data_ = nullptr;
-	}
+	~Matrix();	
 private:
 	size_t rows_;
 	size_t cols_;
